21f64e9cf1e9af00792c15536c62e214dff8ede2
SHAQUIB <SHAQUIB@DESKTOP-3AI5NU9>
2025-10-22 21:31:23 +0530

    Add program of odd and even number

---
+++ DSA-Programs-with-Java/ Basic/com/saqib/basic/EvenAndOddNumber.java
@@ -0,0 +1,13 @@
+package com.saqib.basic;
+
+public class EvenAndOddNumber {
+	public static void main(String[] args) {
+        int number = 29;
+ 
+        if (number % 2 == 0)
+           System.out.println(number + " is Even");
+        else
+           System.out.println(number + " is odd");
+   }
+
+}
