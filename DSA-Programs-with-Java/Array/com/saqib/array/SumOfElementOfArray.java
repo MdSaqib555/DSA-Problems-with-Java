f6007d1622d491ad1e98d4e29c3195ea710788be
SHAQUIB <SHAQUIB@DESKTOP-3AI5NU9>
2025-10-27 00:18:16 +0530

    Add Java program to calculate sum of array

---
+++ DSA-Programs-with-Java/Array/com/saqib/array/SumOfElementOfArray.java
@@ -0,0 +1,16 @@
+package com.saqib.array;
+
+public class SumOfElementOfArray {
+	public static void main(String args[])	   {
+	       int arr[] = {12, 13, 1, 10, 34, 10};
+	       int sum = 0;
+	       for(int i=0; i<arr.length; i++)
+	       {
+	         sum = sum + arr[i];
+	       }
+
+	       System.out.print(sum); 
+	   }
+	}
+
+
