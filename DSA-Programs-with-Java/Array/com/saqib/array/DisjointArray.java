9bd353b5581bd11003f0f661b0bc19f463d0446e
SHAQUIB <SHAQUIB@DESKTOP-3AI5NU9>
2025-10-27 23:38:18 +0530

    Add Java program to check whether array is disjoint or not

---
+++ DSA-Programs-with-Java/Array/com/saqib/array/DisjointArray.java
@@ -0,0 +1,26 @@
+package com.saqib.array;
+
+public class DisjointArray {
+	static boolean Disjoint(int arr1[], int arr2[]) {       
+        for(int i=0; i<arr1.length; i++){
+        for(int j=0; j<arr2.length; j++){
+            if(arr1[i]==arr2[j]){
+                return false;   
+            }
+        }
+    }
+    return true;
+    }
+     public static void main (String[] args)
+    {
+        int arr1[] = {10, 51, 3, 43, 6};
+        int arr2[] = {80, 71, 29, 3};
+        
+        if (Disjoint(arr1, arr2))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
+
+
