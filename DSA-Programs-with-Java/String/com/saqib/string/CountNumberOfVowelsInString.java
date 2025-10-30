2e1a1fbfc40cdc19be90eabfa5bad5e6818c9a11
SHAQUIB <SHAQUIB@DESKTOP-3AI5NU9>
2025-10-30 22:47:23 +0530

    Add Java program to count number of vowels

---
+++ DSA-Programs-with-Java/String/com/saqib/string/CountNumberOfVowelsInString.java
@@ -0,0 +1,17 @@
+package com.saqib.string;
+
+public class CountNumberOfVowelsInString {
+	public static void main(String[] args) {
+		String s = "prepinsta";
+		char[] c = s.toCharArray();
+		int vowel=0;	
+		for (int i = 0; i < s.length(); i++) { 
+            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u') 
+                vowel++; 
+		}
+  
+	System.out.println("Vowels: " + vowel);
+    }
+
+
+}
