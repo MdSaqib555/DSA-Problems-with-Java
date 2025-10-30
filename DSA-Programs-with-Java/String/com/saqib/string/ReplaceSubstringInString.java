73665964eb4534505f4deff40eb40e40aa334d48
SHAQUIB <SHAQUIB@DESKTOP-3AI5NU9>
2025-10-30 23:41:05 +0530

    Add java program to replace substring in string

---
+++ DSA-Programs-with-Java/String/com/saqib/string/ReplaceSubstringInString.java
@@ -0,0 +1,11 @@
+package com.saqib.string;
+
+public class ReplaceSubstringInString {	
+	public static void main(String[] args) {
+        String str = "I love Java programming"; 
+        String result = str.replace("Java", "Python");
+        System.out.println("Original String: " + str);
+        System.out.println("String after replacement: " + result);
+    }
+
+}
